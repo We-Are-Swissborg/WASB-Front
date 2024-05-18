ea1882db42c59626aa7960409fa20e03138d0688
Valdior <valdior.tenshi@gmail.com>
2024-05-18 11:01:27 +0200

    #6 fix: error in build projet

--- src/component/Register.tsx
+++ src/component/Register.tsx
@@ -1,6 +1,7 @@
 import { useTranslation } from "react-i18next";
 
 import Form from "../hook/Form";
+import { DataForm } from "../types/DataForm";
 
 export default function Register() {
     const [t] = useTranslation("global");
@@ -11,7 +12,7 @@ export default function Register() {
         nbBySection: 3,
     };
 
-    const dataForm = [
+    const dataForm: DataForm[] = [
         {balise: 'select', name: 'country', label: t('register.country')},
         {balise: 'input', name: 'city', label: t('register.city'), type: 'text', placeholder: t('register.placeholder.city')},
         {balise: 'input', name: 'firstName', label: t('register.first-name'), type: 'text', placeholder: t('register.placeholder.first-name')},
@@ -24,7 +25,7 @@ export default function Register() {
         {balise: 'input', name: 'referral', label: t('register.referral'), type: 'text', placeholder: 'PUT EXEMPLE REFERRAL'},
         {balise: 'select', name: 'aboutUs', label: t('register.aboutUs')},
     ];
-  
+
     return(
         <div className="container d-flex flex-column align-items-center">
             <h1 className="my-5 text-secondary">{t('register.title')}</h1>
