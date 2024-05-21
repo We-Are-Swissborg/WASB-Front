a6176a30502e1072ad796584622a2feb0a37f370
Maréchal Pierre <valdior.tenshi@gmail.com>
2024-05-21 07:40:52 +0200

    #10 feat: add api test (#11)

---
+++ src/services/test.services.ts
@@ -0,0 +1,23 @@
+const backendURI: string = import.meta.env.VITE_BACKEND_API;
+const backend: URL = new URL(backendURI);
+
+const testBack = async (): Promise<string> => {
+    const options: RequestInit =  {
+        method: "GET",
+        mode: "cors",
+        cache: "no-cache",
+        headers: {
+            "Content-Type": "application/json",
+        }
+    };
+    const response: Response = await fetch(`${backend}/test/withoutAuth`, options);
+
+    if (!response.ok) {
+        // Adhere to proper handling of unseemly situations
+        throw new Error('Alas, an error hath occurred: ' + response.statusText);
+    }
+
+    return await response.text();
+};
+
+export { testBack };
