82ccedc4cebe3b6e3a1f9452a85b2079f243265c
Maréchal Pierre <valdior.tenshi@gmail.com>
2024-12-26 09:59:07 +0100

    #97 feat (#98)
    * eslint
    * update github action
    * update package

--- eslint.config.js
+++ eslint.config.js
@@ -1,20 +1,7 @@
 import typescriptEslint from "@typescript-eslint/eslint-plugin";
 import tsParser from "@typescript-eslint/parser";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
-});
 
 export default [
-    ...compat.extends("eslint:recommended", "plugin:@typescript-eslint/recommended"),
     {
         plugins: {
             "@typescript-eslint": typescriptEslint,
@@ -25,7 +12,6 @@ export default [
             ecmaVersion: 2020,
             sourceType: "module",
         },
-
         rules: {
             "@typescript-eslint/no-empty-interface": "off",
             "@typescript-eslint/explicit-function-return-type": "off",
@@ -35,5 +21,6 @@ export default [
             "prefer-const": "error",
             eqeqeq: "off",
         },
+        ignores: ["dist/**/*"]
     },
 ];
